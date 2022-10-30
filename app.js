57579db62f8fb1f93438db7b16d6f5328805cf97
GEORGE ANKU-MENSAH <92539173+georgeankumensah@users.noreply.github.com>
2022-10-30 09:15:17 -0100

    done

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 
 const app = express();
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send([
+    res.send(
       {
         slackUsername: "George Anku-Mensah",
         backend: true,
         age: 20,
-        bio: "George is a full-stack web developer,who believe he can fix the world using technology.",
+        bio: "George is a full-stack web developer,who believes he can fix the world using technology.",
       },
-    ]);
+    );
 });
 
 app.listen(3000, () =>
-console.log('listening on port 3000'))
+console.log(`listening on port ${port}`))
